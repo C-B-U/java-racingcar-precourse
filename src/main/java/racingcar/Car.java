0b79ef411bf56119eea9882048a1fd8d0c999fe7
Jiyoon Kim <rlawldbs0911@naver.com>
2023-09-06 16:10:17 +0900

    feat(Car): 자동차 이름은 5글자 이하만 입력 가능하도록 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,11 +1,19 @@
 package racingcar;
 
 public class Car {
+    private static final int MAXIMUM_NAME_LENGTH = 5;
     private final String name;
-    private int position = 0;
+    private final int position = 0;
 
     public Car(String name) {
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+        }
     }
 
     // 추가 기능 구현
