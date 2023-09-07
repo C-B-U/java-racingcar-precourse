8f22a770417ac898ddfd49d9b0c4c43c94db4a7b
Jiyoon Kim <rlawldbs0911@naver.com>
2023-09-07 10:58:24 +0900

    feat(RacingController): 자동차들이 전진하는 기능 구현

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,19 +1,38 @@
 package racingcar.controller;
 
 import racingcar.domain.Car;
+import racingcar.domain.RandomNumberGenerator;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class RacingController {
     private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
+    private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
     public void start() {
         outputView.printInputCarNames();
         List<Car> cars = inputView.inputCarNames();
         outputView.printInputNumberOfAttempts();
         int attempts = inputView.inputNumberOfAttempts();
+        process(cars, attempts);
+    }
+
+    private void process(List<Car> cars, int attempts) {
+        outputView.printProcessResultMessage();
+        for (int i = 0; i < attempts; i++) {
+            processRacing(cars);
+            outputView.printNewLine();
+        }
+    }
+
+    private void processRacing(List<Car> cars) {
+        cars.forEach(car -> {
+            car.move(randomNumberGenerator.generate());
+            outputView.printRacingRecord(car);
+        });
     }
 }
--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,12 +2,21 @@ package racingcar.domain;
 
 public class Car {
     private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final int POSSIBLE_MOVE_NUMBER = 4;
     private final String name;
-    private final int position = 0;
+    private int position = 0;
 
     public Car(String name) {
         validateName(name);
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
     }
 
     private void validateName(String name) {
@@ -16,5 +25,9 @@ public class Car {
         }
     }
 
-    // 추가 기능 구현
+    public void move(int number) {
+        if (number >= POSSIBLE_MOVE_NUMBER) {
+            this.position ++;
+        }
+    }
 }
