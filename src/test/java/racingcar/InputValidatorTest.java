13501c907ddb6d79377cb5c51ef14fe5d80fd283
GaBaljaintheroom <junsu1222@naver.com>
2023-09-08 17:01:13 +0900

    test: 자동차 이름이 5자를 초과하면 에러가 발생

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -9,13 +9,19 @@ class InputValidatorTest {
     private static final String ERROR = "[ERROR]";
 
     @Test
-    @DisplayName("쉼표를 포함하여 입력값을 2명이상 입력했는지 확인한다.")
+    @DisplayName("쉼표를 포함하여 입력값을 2명이상 입력하지 않았을 때 에러가 발생한다.")
     void inputContainDivision(){
         InputValidator inputValidator = new InputValidator();
         assertThatThrownBy(() -> inputValidator.validateContainDivision("pobi,"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR);
-
     }
 
+    @Test
+    @DisplayName("자동차 이름이 5자를 초과하면 에러가 발생한다.")
+    void inputCarNamesLength(){
+        assertThatThrownBy(() -> new Car("junsu12"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR);
+    }
 }
