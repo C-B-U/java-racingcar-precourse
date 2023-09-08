70da97e7faa4c71a8dd0eeecc4aa3478232ed83b
GaBaljaintheroom <junsu1222@naver.com>
2023-09-08 20:01:49 +0900

    feat: Cars moveCars 작성

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Cars {
     private static final String DIVISION = ",";
@@ -14,4 +15,14 @@ public class Cars {
        Set<String> carNames = Arrays.stream(name.split(DIVISION)).collect(Collectors.toSet());
        carNames.stream().map(Car::new).forEach(cars::add);
     }
+
+    public void moveCars(int count){
+        IntStream.range(0, count)
+                .forEach(i -> cars.forEach(car -> {
+                    if (CreateRandomValue.canMoveCar()){
+                        car.movePosition();
+                    }
+                }));
+    }
+
 }
