068ebe5a2b6b2940cc6814323536187084cb70f2
김연진 <love_homebody@naver.com>
2023-09-08 17:55:09 +0900

    feat(InputValidator): `,`로 이름을 나누어 입력하지 않은 경우 예외 발생

---
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -0,0 +1,17 @@
+package racingcar.validator;
+
+import racingcar.constant.ExceptionMessage;
+
+import java.util.regex.Pattern;
+
+public class InputValidator {
+
+    private static final String DELIMITER_REGEXP = "^[a-zA-Zㄱ-힣0-9,]*$";
+
+    public void validateDelimiter(String input) {
+        if (!Pattern.matches(DELIMITER_REGEXP, input)) {
+            ExceptionMessage exceptionMessage = ExceptionMessage.INCORRECT_DELIMITER;
+            throw new IllegalArgumentException(exceptionMessage.toString());
+        }
+    }
+}
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.domain.Name;
+import racingcar.validator.InputValidator;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,8 +12,11 @@ public class InputView {
 
     private static final String NAMES_REGEX = ",";
 
+    private final InputValidator inputValidator = new InputValidator();
+
     public List<Name> readNames() {
         String names = Console.readLine();
+        inputValidator.validateDelimiter(names);
         return Arrays.stream(names.split(NAMES_REGEX))
                 .map(Name::new)
                 .collect(Collectors.toList());
