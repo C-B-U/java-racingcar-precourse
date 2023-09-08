762d67143dc82424e56df950c305491c7a26a2e5
김연진 <love_homebody@naver.com>
2023-09-08 18:02:14 +0900

    feat(InputView): 오류 발생 지점부터 다시 입력

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,6 +6,7 @@ import racingcar.validator.InputValidator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class InputView {
@@ -15,16 +16,29 @@ public class InputView {
     private final InputValidator inputValidator = new InputValidator();
 
     public List<Name> readNames() {
-        String names = Console.readLine();
-        inputValidator.validateDelimiter(names);
-        return Arrays.stream(names.split(NAMES_REGEX))
-                .map(Name::new)
-                .collect(Collectors.toList());
+        return attemptedInput(() -> {
+            String names = Console.readLine();
+            inputValidator.validateDelimiter(names);
+            return Arrays.stream(names.split(NAMES_REGEX))
+                    .map(Name::new)
+                    .collect(Collectors.toList());
+        });
     }
 
     public int readTryCount() {
-        String input = Console.readLine();
-        inputValidator.validateIsNumber(input);
-        return Integer.parseInt(input);
+        return attemptedInput(() -> {
+            String input = Console.readLine();
+            inputValidator.validateIsNumber(input);
+            return Integer.parseInt(input);
+        });
+    }
+
+    private <T> T attemptedInput(Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return supplier.get();
+        }
     }
 }
