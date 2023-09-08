862c68328d363c3419f8e2817d8e0a4ee075800b
GaBaljaintheroom <junsu1222@naver.com>
2023-09-08 17:07:22 +0900

    feat: Car validateCarNameLength 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,19 @@
 package racingcar;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        validateCarNameLength(name);
         this.name = name;
     }
 
     // 추가 기능 구현
+    public void validateCarNameLength(String name){
+        if (name.length() > MAX_NAME_LENGTH){
+            throw new IllegalArgumentException(ErrorMessage.NAME_LENGTH_ERROR.getMessage());
+        }
+    }
 }
