1ee97d48bf64e140bcb3d8b73b4a844ff891ac10
GaBaljaintheroom <junsu1222@naver.com>
2023-09-08 19:33:15 +0900

    test: 시도할 횟수가 1회 이상이 아니라면 에러가 발생

--- src/test/java/racingcar/InputValidatorTest.java
+++ src/test/java/racingcar/InputValidatorTest.java
@@ -7,11 +7,11 @@ import static org.assertj.core.api.Assertions.*;
 class InputValidatorTest {
 
     private static final String ERROR = "[ERROR]";
+    InputValidator inputValidator = new InputValidator();
 
     @Test
     @DisplayName("쉼표를 포함하여 입력값을 2명이상 입력하지 않았을 때 에러가 발생한다.")
     void inputContainDivision(){
-        InputValidator inputValidator = new InputValidator();
         assertThatThrownBy(() -> inputValidator.validateContainDivision("pobi,"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR);
@@ -21,6 +21,14 @@ class InputValidatorTest {
     @DisplayName("자동차 이름이 5자를 초과하면 에러가 발생한다.")
     void inputCarNamesLength(){
         assertThatThrownBy(() -> new Car("junsu12"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR);
+    }
+
+    @Test
+    @DisplayName("시도할 횟수가 1회 이상이 아니라면 에러가 발생한다.")
+    void inputTryCount(){
+        assertThatThrownBy(() -> inputValidator.validateTryCount(0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(ERROR);
     }
