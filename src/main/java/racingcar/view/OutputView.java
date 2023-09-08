88a3fded6ee7d6e5f8d80aa46c0edd07547fc998
김연진 <love_homebody@naver.com>
2023-09-08 15:31:39 +0900

    feat(OutputView): 이름 입력 안내 출력 기능 구현

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+import racingcar.constant.OutputMessage;
+
+public class OutputView {
+
+    public void printCarNames() {
+        System.out.println(OutputMessage.READ_NAME);
+    }
+}
