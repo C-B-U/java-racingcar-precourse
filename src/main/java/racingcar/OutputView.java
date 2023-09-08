e40863f0794569997a195ef99e4d4a23d1b08fb5
GaBaljaintheroom <junsu1222@naver.com>
2023-09-08 16:56:12 +0900

    feat: OutputView 생성

---
+++ src/main/java/racingcar/OutputView.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class OutputView {
+    public void printErrorMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printInputCarNames(){
+        System.out.println(ProgressMessage.INPUT_CAR_NAMES);
+    }
+}
