e181959e9a789f26f6d6d21a25340c2ebd27a364
GaBaljaintheroom <junsu1222@naver.com>
2023-09-08 21:14:46 +0900

    test: 게임에서 이긴 최종 우승자의 이름을 나타난다.

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,12 +1,20 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class CarsTest {
 
-    Cars cars = new Cars();
+    private Cars cars;
+
+    @BeforeEach
+    void setUp() {
+        cars = new Cars();
+        cars.addCars("pobi,woni,jun");
+        cars.moveCars();
+    }
 
     @Test
     @DisplayName("자동차를 추가했을 때 차를 움직이면 실행결과에 이름이 나타난다.")
@@ -16,8 +24,16 @@ class CarsTest {
 
         String screen = cars.executionResult();
 
-        assertThat(screen).containsOnlyOnce("pobi :");
-        assertThat(screen).containsOnlyOnce("woni :");
-        assertThat(screen).containsOnlyOnce("jun :");
+        assertThat(screen).containsAnyOf("pobi :", "woni :", "jun :");
+    }
+
+    @Test
+    @DisplayName("게임에서 이긴 최종 우승자의 이름을 나타난다.")
+    void showWinCarNames(){
+        cars.addCars("pobi,woni,jun");
+        cars.moveCars();
+
+        String names = cars.finalWinner();
+        assertThat(names).containsAnyOf("pobi", "woni", "jun");
     }
 }
