6be50c54fff90462e3c962b910d95f5a238a54a5
GaBaljaintheroom <junsu1222@naver.com>
2023-09-08 16:56:28 +0900

    feat: ProgressMessage INPUT_CAR_NAMES 작성

---
+++ src/main/java/racingcar/ProgressMessage.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public enum ProgressMessage {
+
+    INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+    private final String message;
+
+    ProgressMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
